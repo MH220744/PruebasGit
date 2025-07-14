04419f81e8d3d8155197e002208679a8f92e168b
Rodrigo José Miranda Hurtado <rodrigomiranda091@gmail.com>
2025-07-13 18:44:45 -0600

    Agregada carpeta Guia3_MH220744 dentro de guia03

---
+++ guia03/Guia3_MH220744/lista/src/app/page.js
@@ -0,0 +1,16 @@
+import styles from "./page.module.css";
+import Form from "@/components/Form";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className="App">
+    </div>
+    <p>
+      Aqui haremos nuestro TO-DO list
+    </p>
+    <Form>
+    </Form>
+      </main>
+  );
+}
